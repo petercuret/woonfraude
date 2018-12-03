151117e0786eb969875fb928e73f6982df62aa93
Swaan Dekkers <s.dekkers@amsterdam.nl>
2018-12-03 11:52:43 +0100

    opzet features extraction

---
+++ codebase/extract_features.py
@@ -0,0 +1,13 @@
+"""
+extract_features.py
+
+This script takes the enriched BWV data and extract the features 
+that will be used by the model in build_model.py.
+
+Input: enriched BWV data (~38k entries @ 2018-11-21)
+Output: data of features used to make a model
+
+Written by Swaan Dekkers & Thomas Jongstra
+"""
+
+# Source this script from collect_data_and_make_model.ipynb.
